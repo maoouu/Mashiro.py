5f43120e4e5010850aae8ca0443c89d52dea3d7b
Maou <alwayscreature@gmail.com>
2021-03-04 08:04:57 +0000

    deployed Mashiro.py to repl.it

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import keep_alive
 
 from utils import default
 from utils.data import Bot, HelpFormat
@@ -9,7 +10,7 @@ print("Logging in...")
 
 bot = Bot(
     command_prefix=config["prefix"], prefix=config["prefix"],
-    owner_ids=config["owners"], command_attrs=dict(hidden=True), help_command=HelpFormat(),
+    owner_ids=os.environ.get("OWNER"), command_attrs=dict(hidden=True), help_command=HelpFormat(),
     intents=discord.Intents(
         # kwargs found at https://discordpy.readthedocs.io/en/latest/api.html?highlight=intents#discord.Intents
         guilds=True, members=True, messages=True, reactions=True, presences=True
@@ -22,6 +23,9 @@ for file in os.listdir("cogs"):
         bot.load_extension(f"cogs.{name}")
 
 try:
-    bot.run(config["token"])
+    # run the server
+    keep_alive.keep_alive()
+    #run the bot
+    bot.run(os.environ.get("BOT_TOKEN"))
 except Exception as e:
     print(f'Error when logging in: {e}')
